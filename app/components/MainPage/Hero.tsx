145989c0d7001f47d23cee9bedd1b523a75c317c
Hilmi Wismadi <dzakiwismadi@gmail.com>
2024-01-08 23:09:09 +0700

    feat: testing hero component

---
+++ app/components/MainPage/Hero.tsx
@@ -0,0 +1,28 @@
+export default function Hero() {
+  return (
+    <section className="w-full min-h-screen relative bg-[#E2E8F0]  flex flex-row justify-center items-center">
+      <div className="text-black flex ">
+        <div>
+          <div className="text-[1.6vw]">Arachnova </div>
+          <div className="text-[1.6vw]">Your Web Design Partner</div>
+          <div className="text-[2.3vw]">Always Dependable</div>
+          <div>Crafting Digital Presence in Every Strand of Code</div>
+        </div>
+        <div>
+            <div className="bg-gray-400 w-[28vw] h-[36vh] rounded-lg border-4 border-red-600 ">
+                <div>
+                  <p>import "./globals.css";</p>  
+                  <p>import &#123; ArachnoVa &#125;  from "./api/provider</p>  
+                  <p> &nbsp; </p>  
+                  <p>export default</p>  
+                  <p>&lt;div className="Welcome to ArachnoVa"&gt;</p>  
+                  <p>import "./globals.css";</p>  
+                  <p>import "./globals.css";</p>  
+                  <p>import "./globals.css";</p>  
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+}
