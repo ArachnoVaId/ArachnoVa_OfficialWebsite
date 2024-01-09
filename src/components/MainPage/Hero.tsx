99c2fcbb7ff2bce526ca7efc3f4529a86edca355
deren21 <109866900+deren21@users.noreply.github.com>
2024-01-09 13:55:01 +0700

    <fix: recover hero code>

--- src/components/MainPage/Hero.tsx
+++ src/components/MainPage/Hero.tsx
@@ -7,55 +7,63 @@ export default function Hero() {
         className="absolute w-full h-full -z-[1] max-lg:hidden "
         draggable="false"
       />
-      <div className="text-black flex">
+      <div className="text-black flex gap-x-[8vw]">
         <div className="flex flex-col justify-center">
           <div className="text-[1.6vw]">Arachnova </div>
           <div className="text-[2vw]">Your Web Design Partner</div>
           <div className="text-[2.8vw]">Always Dependable</div>
-          <div className="text-[#4E5F66]">
+          <div className="text-[1vw] text-[#4E5F66]">
             Crafting Digital Presence in Every Strand of Code
           </div>
-          <div>
-            <div className="w-[4vw] h-[4vh] bg-[#2EC1D9]"></div>
+          <div className="py-[2vh] flex gap-[1vw]">
+            <div className="w-[8vw] h-[4vh] bg-[#2EC1D9] text-white rounded-md text-[0.8vw] flex justify-center items-center ">
+              Start Your Project
+            </div>
+            <div className="w-[10vw] h-[4vh] bg-transparent text-[#1CA7BD] rounded-md text-[0.8vw] flex justify-center items-center">
+              Discover Our Services
+            </div>
           </div>
         </div>
-        <div>
-          <div className="bg-transparent backdrop-blur-lg w-[28vw] h-[40vh] rounded-lg border-4 border-[#F1F5F9] ml-[4vw] ">
-            <div className="px-[1vw] py-[4vh]">
-              <p className="text-[#0151EC]">
-                import <span className="text-[#8131B2]"> "./globals.css";</span>
-              </p>
-              <p className="text-[#1E293B]">
-                <span className="text-[#0151EC]">import</span> &#123;{" "}
-                <span className="text-[#1CA7BD]"> ArachnoVa</span> &#125; from{" "}
-                <span className="text-[#8131B2]">"./api/provider</span>;
-              </p>
-              <p> &nbsp; </p>
-              <p className="text-[#0151EC]">export default</p>
-              <p className="text-[#1E293B]">
-                &lt;div <span className="text-[#0151EC]">className</span>="
-                <span className="text-[#8131B2]">Welcome to ArachnoVa</span>
-                "&gt;
-              </p>
-              <p>
-                &nbsp; &lt;div <span className="text-[#0151EC]">className</span>
-                ="<span className="text-[#8131B2]">content</span>"&gt;
-              </p>
-              <p>&nbsp; &#123;</p>
-              <p>
-                &nbsp; &nbsp;{" "}
-                <span className="text-[#8131B2]">
-                  "Your web design partner"
-                </span>
-              </p>
-              <p>&nbsp; &#125;</p>
-              <p>&nbsp; &lt;/div&gt;</p>
-              <p>
-                &nbsp; &lt;<span className="text-[#0151EC]">img</span> src="
-                <span className="text-[#8131B2]">img/herofix.png</span>"/&gt;
-              </p>
-              <p>&lt;/div&gt;</p>
-            </div>
+
+        <div className="bg-transparent backdrop-blur-lg w-[28vw] h-[20vw] rounded-lg border-2 border-[#F1F5F9] relative ">
+          <div className="absolute w-[20vw] h-[4vw] bg-white rounded-md -bottom-[2vw] -right-[2vw] text-[1vw] flex justify-center items-center border-2 border-[#E2E8F0] gap-x-[0.4vw]">
+            <span className="text-[#8131B2]">@arachnova</span>
+            <span className="text-[#0151EC]">official-site</span>
+            <span className="text-[#1CA7BD]">@</span>
+            npm run dev
+          </div>
+          <div className="px-[1vw] py-[2vw] text-[1vw]/[1.3vw]">
+            <p className="text-[#0151EC]">
+              import <span className="text-[#8131B2] "> "./globals.css";</span>
+            </p>
+            <p className="text-[#1E293B]">
+              <span className="text-[#0151EC]">import</span> &#123;{" "}
+              <span className="text-[#1CA7BD]"> ArachnoVa</span> &#125; from{" "}
+              <span className="text-[#8131B2]">"./api/provider</span>;
+            </p>
+            <p> &nbsp; </p>
+            <p className="text-[#0151EC]">export default</p>
+            <p className="text-[#1E293B]">
+              &lt;div <span className="text-[#0151EC]">className</span>="
+              <span className="text-[#8131B2]">Welcome to ArachnoVa</span>
+              "&gt;
+            </p>
+            <p>
+              &nbsp; &lt;div <span className="text-[#0151EC]">className</span>
+              ="<span className="text-[#8131B2]">content</span>"&gt;
+            </p>
+            <p>&nbsp; &#123;</p>
+            <p>
+              &nbsp; &nbsp;{" "}
+              <span className="text-[#8131B2]">"Your web design partner"</span>
+            </p>
+            <p>&nbsp; &#125;</p>
+            <p>&nbsp; &lt;/div&gt;</p>
+            <p>
+              &nbsp; &lt;<span className="text-[#0151EC]">img</span> src="
+              <span className="text-[#8131B2]">img/herofix.png</span>"/&gt;
+            </p>
+            <p>&lt;/div&gt;</p>
           </div>
         </div>
       </div>
